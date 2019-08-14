3442979424bf36001d0dc4c2df3f7432bf5b509c
dbould <dave@bould.net>
2019-08-14 15:16:01 +0100

    Add in skeleton code for commands

--- public/index.php
+++ public/index.php
@@ -8,19 +8,8 @@ if (PHP_SAPI == 'cli-server') {
         return false;
     }
 }
-require __DIR__ . '/../vendor/autoload.php';
-session_start();
-// Instantiate the app
-$settings = require __DIR__ . '/../src/settings.php';
-$app = new \Slim\App($settings);
-// Set up dependencies
-$dependencies = require __DIR__ . '/../src/dependencies.php';
-$dependencies($app);
-// Register middleware
-$middleware = require __DIR__ . '/../src/middleware.php';
-$middleware($app);
-// Register routes
-$routes = require __DIR__ . '/../src/routes.php';
-$routes($app);
+
+require_once __DIR__ . '/../bootstrap.php';
+
 // Run app
 $app->run();
